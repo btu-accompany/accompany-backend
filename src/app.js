04265da31382a085ab83f782c6519b70f2a14aa1
MustafaErenn <18360859024@ogrenci.btu.edu.tr>
2022-05-10 10:31:11 +0300

    mongodb baglantısı kuruldu

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+require("dotenv/config");
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Ana Sayfa");
 });
+
+
+//Connect DB 
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    () => {
+        console.log("Connected To Db");
+    }
+);
 
 app.listen(port, () => {
     console.log(port + ". portta çalışıyor");
